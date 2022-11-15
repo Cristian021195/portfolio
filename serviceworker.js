dbe61fb313932019c4f071148c99da2b70b9e223
cristian021195 <cristiangramajo015@gmail.com>
2022-11-15 16:03:12 -0300

    se agrego service worker para cacheo de imagenes

---
+++ serviceworker.js
@@ -0,0 +1,31 @@
+// SW Chárlalo v.2
+
+const CACHE_NAME = "portfolio-v1";
+const urlsToCache = ["/", "./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "./img/dev-icon.png", "./img/favicon.svg",
+"./img/svg/adobe-xd.svg","./img/svg/bootstrap5.svg","./img/svg/climap.svg","./img/svg/CSS3.svg","./img/svg/escritos.svg","./img/svg/figma.svg",
+"./img/svg/horabondi.svg","./img/svg/HTML5.svg","./img/svg/javascript.svg","./img/svg/jquery.svg","./img/svg/languaje.svg","./img/svg/lista-compras.svg",
+"./img/svg/materialize.svg","./img/svg/mysql-2.svg","./img/svg/nodejs.svg","./img/svg/notas.svg","./img/svg/office.svg","./img/svg/panerita.svg",
+"./img/svg/php-1.svg","./img/svg/postman.svg","./img/svg/react-black.svg"];
+
+self.addEventListener("install", event => {
+    // Pre-cache archivos para offline y performance; los pictogramas los dejamos para la primera carga
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache)
+        })
+    );
+});
+
+// Cache First Policy
+self.addEventListener("fetch", event => {
+  event.respondWith(
+      caches.match(event.request)  
+          .then(function(response) {
+              if (response) {  // CACHE HIT
+                  return response;
+              } else {    // CACHE MISS
+                  return fetch(event.request);
+              }
+          })
+  );
+});
