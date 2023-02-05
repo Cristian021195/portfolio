6c086720be15e8c85001bb2646163a118853c6cf
cristian021195 <cristiangramajo015@gmail.com>
2023-02-05 09:56:47 -0300

    link linkedin modificado

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "portfolio-v16";
+const CACHE_NAME = "portfolio-v17";
 const urlsToCache = ["./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "./img/favicon.svg",
 "./img/svg/adobe-xd.svg","./img/svg/bootstrap5.svg","./img/svg/climap.svg","./img/svg/CSS3.svg","./img/svg/escritos.svg","./img/svg/figma.svg",
 "./img/svg/horabondi.svg","./img/svg/horabondi-beta.svg","./img/svg/HTML5.svg","./img/svg/javascript.svg","./img/svg/jquery.svg","./img/svg/astro.svg","./img/svg/languaje.svg","./img/svg/lista-compras.svg",
@@ -6,6 +6,8 @@ const urlsToCache = ["./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.wo
 "./img/svg/php-1.svg","./img/svg/postman.svg","./img/svg/react-black.svg", "./img/horizontal.webp", "./img/vertical.webp"];
 
 self.addEventListener("install", event => {
+    // forzado de instalacion de nuevo sw
+    self.skipWaiting();
     // Pre-cache archivos para offline y performance; los pictogramas los dejamos para la primera carga
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
