fb6dca62b08876ea2cc6fb6aee9e3e496d952627
cristian021195 <cristiangramajo015@gmail.com>
2023-02-05 10:11:40 -0300

    cambios en sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = "portfolio-v17";
+const CACHE_NAME = "portfolio-v20";
 const urlsToCache = ["./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "./img/favicon.svg",
 "./img/svg/adobe-xd.svg","./img/svg/bootstrap5.svg","./img/svg/climap.svg","./img/svg/CSS3.svg","./img/svg/escritos.svg","./img/svg/figma.svg",
 "./img/svg/horabondi.svg","./img/svg/horabondi-beta.svg","./img/svg/HTML5.svg","./img/svg/javascript.svg","./img/svg/jquery.svg","./img/svg/astro.svg","./img/svg/languaje.svg","./img/svg/lista-compras.svg",
 "./img/svg/materialize.svg","./img/svg/mysql-2.svg","./img/svg/radio-app.svg","./img/svg/nodejs.svg","./img/svg/notas.svg","./img/svg/office.svg","./img/svg/panerita.svg",
 "./img/svg/php-1.svg","./img/svg/postman.svg","./img/svg/react-black.svg", "./img/horizontal.webp", "./img/vertical.webp"];
+
+// si cambiamos la version de cache se elimina la anterior
+caches.keys().then(res=>{
+    res.forEach(e=>{
+        if(e != CACHE_NAME){
+            caches.delete(e)
+        }
+    })
+})
 
 self.addEventListener("install", event => {
     // forzado de instalacion de nuevo sw
