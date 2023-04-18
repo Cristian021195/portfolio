422626fe331993d138dca878fd5d2f2a7ec80d53
cristian021195 <cristiangramajo015@gmail.com>
2023-04-18 18:08:32 -0300

    proyecto colores agregado

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "portfolio-v22";
+const CACHE_NAME = "portfolio-v23";
 const urlsToCache = ["./css/BebasNeue-Regular.woff", "./css/BebasNeue-Regular.woff2", "./img/favicon.svg",
 "./img/svg/adobe-xd.svg", "./img/svg/romano.svg","./img/svg/bootstrap5.svg","./img/svg/climap.svg","./img/svg/CSS3.svg","./img/svg/escritos.svg","./img/svg/figma.svg",
 "./img/svg/horabondi.svg","./img/svg/horabondi-beta.svg","./img/svg/HTML5.svg","./img/svg/javascript.svg","./img/svg/jquery.svg","./img/svg/astro.svg","./img/svg/languaje.svg","./img/svg/lista-compras.svg",
 "./img/svg/materialize.svg","./img/svg/mysql-2.svg","./img/svg/radio-app.svg","./img/svg/nodejs.svg","./img/svg/notas.svg","./img/svg/office.svg","./img/svg/panerita.svg",
-"./img/svg/php-1.svg","./img/svg/postman.svg","./img/svg/react-black.svg", "./img/horizontal.webp", "./img/vertical.webp"];
+"./img/svg/php-1.svg","./img/svg/postman.svg","./img/svg/react-black.svg", "./img/horizontal.webp", "./img/vertical.webp", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Colorwheel.svg"];
 
 // si cambiamos la version de cache se elimina la anterior
 caches.keys().then(res=>{
